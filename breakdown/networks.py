c51296a7dcc791658bfa009a1f394175e8bc3551
chaddy <cpaik1004@gmail.com>
2020-12-21 15:34:52 +0900

    start on input embedding example

--- breakdown/networks.py
+++ breakdown/networks.py
@@ -4,17 +4,18 @@ from torch.nn import Module, ModuleDict, ModuleList
 
 d_model = 512
 n_heads = 6
-a = nn.TransformerEncoderLayer(d_model=d_model, nhead=n_heads)
-
-
-class Attention(Module):
-    def __init__(self, q, k, v):
-        super().__init__()
-        pass
-
-    def forward(self):
-
-
-
-class TransformerEncoderLayerScratch(Module):
-
+# a = nn.TransformerEncoderLayer(d_model=d_model, nhead=n_heads)
+b = nn.Embedding(num_embeddings=512, embedding_dim=4)
+print(b.training) # wanted to check if the embedding layer gets trained
+#
+# class Attention(Module):
+#     def __init__(self, q, k, v):
+#         super().__init__()
+#         pass
+#
+#     def forward(self):
+#
+#
+#
+# class TransformerEncoderLayerScratch(Module):
+#
